b4c40e2db018bfff2cfd4d44d8ec7a9c8b136b53
Roy Penrod <roypenrod123@yahoo.com>
2016-01-04 22:28:02 -0600

    serialized Meetup API call with Step so we could access data

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var Step = require('step');
 
-function getUpcomingMeetups(groupUrlName) {
-        
-    var reqString = 'https://api.meetup.com/2/events?group_urlname=';
-    reqString += groupUrlName;
-    reqString += '&status=upcoming&page=1';
-    
-    // uses the request Node library to send the get request
-    request.get(reqString, function(error, response, jsonData) {
-                
-        // if successful, return the jsonData
-        // else, return an an object with error info
-        if (!error && response.statusCode == 200) {            
-            return jsonData;
-        } else {
-            var uhOh = {
-                errorMessage: error,
-                statusCode: response.statusCode
-            };
-            
-            return uhOh;
-        }
-    });     
-}
+var OUR_GROUP_URL_NAME = 'Free-Code-Camp-NWA';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -33,9 +13,69 @@ router.get('/', function(req, res, next) {
 
 // /* GET meetup page */
  router.get('/meetup', function(req, res) {
-    console.log(getUpcomingMeetups('Free-Code-Camp-NWA'));
     
-    res.render('meetup');
+    // use Step to serialize the API call
+    // so we don't try to process data until
+    // we've received it
+    Step(        
+        function getMeetupData() {
+                        
+            var self = this;
+            
+            var meetupApiUrl = 'https://api.meetup.com/2/events?group_urlname='
+            + OUR_GROUP_URL_NAME 
+            + '&status=upcoming&page=1';
+            
+            // uses the request Node library to send the get request
+            request.get(meetupApiUrl, function(error, response, jsonData) {
+         
+                // if successful, pass along the jsonData
+                // else, pass along an error
+                if (!error && response.statusCode == 200) {            
+                    self(null, jsonData);
+                } else {
+                    self(error, jsonData);
+                }
+            }); 
+        },
+        function processMeetupData(err, meetupDataJSON) {
+            
+            // Uh oh, there's an error. 
+            // Render the error template.
+            if (err !== null) {
+                
+                res.render('error', {
+                    message: 'Error getting data from Meetup.',
+                    error: err
+                });
+            // No error.
+            // Render the meetup template.
+            } else {
+                                
+                // throws error on 
+                // meetupDAtaJSON.results[0].name
+                // Why can't I access this???
+                console.log('+++++++++++++++++++++');
+                console.log(' ');                
+                console.log("meetupDataJSON: ");
+                console.log(meetupDataJSON);
+                console.log(' ');
+                // console.log(meetupDataJSON.results[0].name);
+                console.log('+++++++++++++++++++++');
+            
+
+                res.status(200);
+                res.type('json');
+                res.send(meetupDataJSON);
+                res.end();
+                
+                // Just need to grab data out of meetupDataJSON
+                // and get it into the view.
+            }       
+        }
+    );
+    
+    
  });
 
 module.exports = router;
