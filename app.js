98106e0575738b28ab4c76bf8dbef14e59bb1493
Roy Penrod <roypenrod123@yahoo.com>
2016-01-04 20:26:07 -0600

    added Matt's code to the getUpcomingMeetups() function in index.js -- cleaned up files we didn't need

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
-//var fbook = require('./routes/fbook');
-//var meetup = require('./routes/meetup');
+
 var app = express();
 
 // view engine setup
@@ -26,9 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
-//app.use('/fbook', fbook);
-//app.use('/meetup', meetup);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,40 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-//Use request to get meetup event api. next step is to figure out the best way to parse the JSON response into a solution for views to use.
-request.get('https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=Free-Code-Camp-NWA&page=20', function(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body)
-
-  }
-})
+function getUpcomingMeetups(groupUrlName) {
+        
+    var reqString = 'https://api.meetup.com/2/events?group_urlname=';
+    reqString += groupUrlName;
+    reqString += '&status=upcoming&page=1';
+    
+    // uses the request Node library to send the get request
+    request.get(reqString, function(error, response, jsonData) {
+                
+        // if successful, return the jsonData
+        // else, return an an object with error info
+        if (!error && response.statusCode == 200) {            
+            return jsonData;
+        } else {
+            var uhOh = {
+                errorMessage: error,
+                statusCode: response.statusCode
+            };
+            
+            return uhOh;
+        }
+    });     
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-/* GET fbook page */
-router.get('/fbook', function(req, res) {
-    res.render('fbook');
-});
-
-// /* GET fbook page */
+// /* GET meetup page */
  router.get('/meetup', function(req, res) {
-   res.render('meetup');
+    console.log(getUpcomingMeetups('Free-Code-Camp-NWA'));
+    
+    res.render('meetup');
  });
 
 module.exports = router;
