b820497dec5762d018eef70c6a72f20af647d52f
Matt Luker <matt.luker001@gmail.com>
2016-01-11 23:01:03 -0600

    testing github desktop

--- routes/index.js
+++ routes/index.js
@@ -14,15 +14,15 @@ var OUR_GROUP_URL_NAME = 'Free-Code-Camp-NWA';
 //   min:  mm
 //   period: am | pm
 function convertMilitaryTime(time) {
-            
+
     // splits the time into an array using the colon as separator
     var splitTime = time.split(':');
-        
+
     // converts the strings to integers
     var hour = parseInt(splitTime[0]);
     var min = splitTime[1];
     var period = '';
-    
+
     // converts the hour from military time to standard time
     if (hour === 0) {
         hour = 12;
@@ -35,14 +35,14 @@ function convertMilitaryTime(time) {
         hour = hour - 12;
         period = 'pm';
     }
-    
+
     // creates the converted time object and returns it
     var convertedTime = {
         hour: hour.toString(),
         min: min,
-        period: period        
+        period: period
     };
-    
+
     return convertedTime;
 }
 
@@ -54,37 +54,37 @@ router.get('/', function(req, res, next) {
 
 // /* GET meetup page */
  router.get('/meetup', function(req, res) {
-    
+
     // use Step to synchronize the API call
     // so we don't try to process data until
     // we've received it
-    Step(        
+    Step(
         function getMeetupData() {
-                        
+
             var self = this;
-            
+
             var meetupApiUrl = 'https://api.meetup.com/2/events?group_urlname='
-            + OUR_GROUP_URL_NAME 
+            + OUR_GROUP_URL_NAME
             + '&status=upcoming&page=1';
-            
+
             // uses the request Node library to send the get request
             request.get(meetupApiUrl, function(error, response, jsonData) {
-         
+
                 // if successful, pass along the jsonData
                 // else, pass along an error
-                if (!error && response.statusCode == 200) {            
+                if (!error && response.statusCode == 200) {
                     self(null, jsonData);
                 } else {
                     self(error, jsonData);
                 }
-            }); 
+            });
         },
         function processMeetupData(err, meetupDataJSON) {
-           
-            // Uh oh, there's an error. 
+
+            // Uh oh, there's an error.
             // Render the error template.
             if (err !== null) {
-                
+
                 res.render('error', {
                     message: 'Error getting data from Meetup.',
                     error: err
@@ -92,12 +92,12 @@ router.get('/', function(req, res, next) {
             // No error.
             // Render the meetup template.
             } else {
-                            
-                // need to parse the JSON to de-serialize it 
+
+                // need to parse the JSON to de-serialize it
                 // from a string to an object we can work with
                 // Thank you, Blake. -- Roy :-)
                 dataJSON = JSON.parse(meetupDataJSON);
-                
+console.log(dataJSON);
                 // meetup time is in format of milliseconds since epoch
                 // convert to local date time
                 var msUTC = dataJSON.results[0].time;
@@ -105,7 +105,7 @@ router.get('/', function(req, res, next) {
                 var localDateTime = new Date(0);
                 // add the number of milliseconds to it to get the local date time
                 localDateTime.setMilliseconds(msUTC);
-                
+
                 // break the localDateTime down into separate variables
                 localDateTime = localDateTime.toString();
                 var splitDateTime = localDateTime.split(' ');
@@ -113,23 +113,23 @@ router.get('/', function(req, res, next) {
                 var eventMonth = splitDateTime[1];
                 var eventDay = splitDateTime[2];
                 var eventYear = splitDateTime[3];
-                
+
                 // time is in military time
                 // break the event time down into hours, minutes, and period
                 var eventTime = convertMilitaryTime(splitDateTime[4]);
-                                
+
                 // meetup event duration is in the format of milliseconds
-                // convert to hours                
+                // convert to hours
                 var duration = ((dataJSON.results[0].duration / 1000) / 60) / 60;
-                
+
                 // send the raw JSON data to the browser
                 // res.status(200);
                 // res.type('json');
                 // res.send(meetupDataJSON);
                 // res.end();
-                            
-                
-                // send the data to the view with variables                
+
+
+                // send the data to the view with variables
                 res.render('meetup', {name: dataJSON.results[0].name,
                                       description: dataJSON.results[0].description,
                                       eventURL: dataJSON.results[0].event_url,
@@ -151,11 +151,11 @@ router.get('/', function(req, res, next) {
                                       eventStartMin: eventTime.min,
                                       eventStartPeriod: eventTime.period
                                      });
-            }       
+            }
         }
     );
-    
-    
+
+
  });
 
 module.exports = router;
